52ab5d3d5379abeca9c32ebef62f27cf49952b28
Dario Costa <dott.dario.costa@gmail.com>
2021-11-07 01:57:02 +0100

    Fatta pagina community, ma ha un bug che non riesco ad individuare

--- Frontend/src/app/routes/community/community.component.ts
+++ Frontend/src/app/routes/community/community.component.ts
@@ -7,6 +7,7 @@ import { MoviesApiService } from '../../services/moviesapi.service';
 import { UsersManagerApiservice } from '../../services/usersManagerApi.service';
 import { DataInterface, MovieRatingInterface } from '../../models/movieRating.model';
 import { ResultInterface } from '../../models/apiMovie.model';
+import { UserInterface } from '../../models/apiUsers.model';
 
 @Component({
   selector: 'app-community',
@@ -18,11 +19,19 @@ export class CommunityComponent implements OnInit {
 
     //variabili
     userId: number;
-    movieRating: MovieRatingInterface;
-    data: DataInterface [];
+    movieRatings: MovieRatingInterface;
+    user: UserInterface;
+    data: DataInterface;
+    datas: DataInterface [];
     titles: string[] = [];
+    userNames: string [] = [];
+    ratings: number[] = [];
     movie: ResultInterface;
-
+    userIds: number [] = [];
+    movieIds: number [] = [];
+    titleMap = new Map<number, string>();
+    userMap = new Map<number, string>();
+ 
     constructor(private dataService: DataService, private router: Router,
     private ratingService: MovieRatingService, private commentService: CommentsApiService,
     private movieService: MoviesApiService, private userService: UsersManagerApiservice) { }
@@ -33,27 +42,69 @@ export class CommunityComponent implements OnInit {
     // this.getEntryCommentService();
   }
 
-  //Mostra i movies della web API
-  getEntriesMovieApi(id) {
-    this.movieService.getMovieByMovieId(id).subscribe((response) => {
-      this.movie = response;
-      console.log(this.movie)
-      this.titles.push(this.movie.title)
-      })
-  }
+    //Mostra i rating memorizzati dal BE Laravel - db localhost:8000
+    getEntryRatingService() {
+      this.ratingService.getMovieRating().subscribe((res: any) => {
+        this.movieRatings = res;
+        this.datas = this.movieRatings.data;
+        this.userIds = this.datas.map(a => a.user_id)
+        this.movieIds = this.datas.map(b => b.movie_id)
+        this.ratings = this.datas.map(c => c.movie_rating)
+        console.log(this.datas)
+        console.log("this.userIds: ", this.userIds)
+        console.log("this.movieIds: ", this.movieIds)
+        console.log("this.ratings: ", this.ratings)
+        this.getMovieTitles(this.movieIds);
+           }
+      )
+    }
+  
+      //Richiama i movies corrispondenti all'id passato e ne estrae il titolo
+      getMovieTitles(movieIds : number []) {
+        var x = 0;
+        for (let movieId of movieIds){
+        this.movieService.getMovieByMovieId(movieId).subscribe((response) => {
+          this.movie = response;
+          console.log ("this.movie: ", this.movie.title)
+          this.titleMap.set(movieId, this.movie.title);
+          x++;
+          if (x == movieIds.length){
+            console.log(this.titleMap)
+            for (let movieId of this.movieIds) {
+              this.titles.push(this.titleMap.get(movieId)) // Get a movie title mapped with given movie ID and push it in a string array
+              console.log("Titles: ", this.titles)
+              this.getUsernames(this.userIds);
+            }
+          }
+        }, error => {
+          this.titles.push("(unknown movie)")
+          x++;
+        })
+      }
+    }
 
-  //Mostra i rating memorizzati dal BE Laravel - db localhost:8000
-  getEntryRatingService() {
-    this.ratingService.getMovieRating().subscribe((res: any) => {
-      this.movieRating = res;
-      this.data = this.movieRating.data;
-      console.log(this.data)
-      for (let i = 0; i < this.data.length; i++) {
-        this.getEntriesMovieApi(this.data[i].movie_id)
-         }
-    })
-  }
-
+    //Richiama gli utenti corrispondenti all'id passato e ne estrae il nome
+    getUsernames(userIds) {
+      var x = 0; // Set counter variable
+      for (let userId of this.userIds) {
+        this.userService.getUserById(userId).subscribe((res: any) => {
+          x++; // Increase counter
+          this.user = res;
+          this.userMap.set(userId, this.user.username);
+          if (x == this.userIds.length) {   // When for loop is at it lasts instance
+            for (let userId of this.userIds) {
+              if (this.userMap.get(userId) != undefined){
+              this.userNames.push(this.userMap.get(userId)) // Get a username mapped with given user ID and push it in a string array
+              console.log("Usernames: ", this.userNames)
+              } else {
+                this.userNames.push("(unknown user)")
+              }
+            }
+          }
+        })
+        }
+    }
+  
   // //Mostra i commenti memorizzati dal BE Dotnet - db localhost:5000
   // getEntryCommentService() {
   //   this.commentService.getAllCommentsByMovieId(this.userId).subscribe((response: any) => {
